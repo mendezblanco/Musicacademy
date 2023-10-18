c9c140955c93fc8b495eb12f417ae674671fc6c0
Alex <2796748641706@ingenieria.usac.edu.gt>
2023-10-18 16:59:16 -0600

    Prueba2

--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -128,7 +128,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Music_academy',
         'USER': 'postgres',
-        'PASSWORD': 'andre123',
+        'PASSWORD': 'alexj2020',
         'HOST': '127.0.0.1',
         'PORT': '5432',
 
